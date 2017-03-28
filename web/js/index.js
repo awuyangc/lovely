bb2a79865b729e5ec128356d3243dfb8cdea09e8
wuyang <289677561@qq.com>
2017-03-28 17:07:08 +0800

    20170327_1639

---
+++ web/js/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/3/28.
+ */
+$(function(){
+    // window.onscroll=function(){
+    //     var sl=-Math.max(document.body.scrollLeft,document.documentElement.scrollLeft);
+    //     document.getElementById('wy-navbar').style.left=sl+'px';
+    // }
+
+});
